f3ffa5dc9408559ae589ecdc21f13a62d7198033
igor.kudlo <igoreklida@gmail.com>
2024-11-24 16:08:45 +0300

    Categories, filter, sort, slider views

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import {Container, Filters, Title, TopBar} from "@/components/shared";
+
 export default function Home() {
   return (
-    <div>Главная</div>
+    <>
+      <Container className="mt-5">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,9 @@
+export { Categories } from './categories';
+export { CheckboxFiltersGroup } from './checkbox-filters-group';
 export { Container } from './container';
+export { FilterCheckbox } from './filter-checkbox';
+export { Filters } from './filters';
 export { Header } from './header';
+export { SortPopup } from './sort-popup';
+export { Title } from './title';
+export { TopBar } from './top-bar';
