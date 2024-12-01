88984ec4baf666f3109fce45bec25cc2f166d7ff
igor.kudlo <igoreklida@gmail.com>
2024-12-01 15:19:56 +0300

    products list

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import {Container, Filters, Title, TopBar} from "@/components/shared";
+import {ProductCard} from "@/components/shared/product-card";
+import {ProductsGroupList} from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -10,9 +12,21 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList title={'Пицца1'} items={[{
+                id: 1,
+                name: 'XXX',
+                imageUrl: '/',
+                price: 100,
+                items: [{price: 550}]
+              }]} categoryId={1} />
+            </div>
           </div>
         </div>
       </Container>
